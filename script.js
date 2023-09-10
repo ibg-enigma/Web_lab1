7f4956ed7680bf8fdb4ff49d83583223bcedee8f
Enigma <linagol.akiaer@mail.ru>
2023-09-10 22:43:32 +0300

    Release

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 function openOrCloseMenu(evt) {
     var menu = document.getElementById("Hidden");
 
-    if (menu.style.display == "none") {
-        menu.style.display = "block";
+    if (menu.classList.contains("hidden-list")) {
+        menu.classList.remove("hidden-list");
     } else {
-        menu.style.display = "none";
+        menu.classList.add("hidden-list");
     }
 }
 
 function openWindow(evt, contentType) {
     var btns = document.getElementsByClassName("page-navigation-button");
-    for (i = 0; i < btns.length; i++) {
+    for (var i = 0; i < btns.length; i++) {
         var btn = btns.item(i);
         btn.classList.remove("bold-button");
     }
 
-    var button = document.getElementById(contentType);
-    button.classList.add("bold-button");
+    evt.currentTarget.classList.add("bold-button");
+    
+    var info_items = document.getElementsByClassName("information-item");
+    for (var i = 0; i < info_items.length; i++) {
+        info_items[i].style.display = "none";
+    }
+
+    var content = document.getElementsByClassName(contentType);
+    if (content.length == 0) return;
+
+    for (var i = 0; i < content.length; i++) {
+        content[i].style.display = "block";
+    }
 }
